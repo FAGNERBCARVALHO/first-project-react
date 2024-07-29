e5face6cdddcb01b94bd603d2974c39bb59e7b50
FAGNERBCARVALHO <fa.carvalho117@gmail.com>
2024-07-28 21:49:56 -0300

    props in react

--- src/styles.js
+++ src/styles.js
@@ -7,18 +7,25 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+`
 export const TodoList = styled.div`
   background: white;
   padding: 30px 20px;
-`;
+
+  ul {
+    padding:0;
+    margin-top: 60px;
+
+  }
+`
 
 export const Input = styled.input`
   border: 2px solid rgba(209, 211, 212, 0.4);
   border-radius: 5px;
   height: 40px;
-  margin-right: 10px;
-`;
+  margin-right: 40px;
+  width: 340px;
+`
 
 export const Button = styled.button`
   background: #8052ec;
@@ -26,13 +33,26 @@ export const Button = styled.button`
   font-weight: 900;
   font-size: 17px;
   line-height: 2px;
+  width:130px;
   height: 40px;
   border: none;
   color: white;
-`;
+
+`
 export const ListItem = styled.div`
   background: #e4e4e4;
   box-shadow: 1px 4px 10px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   height: 60px;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  padding: 0 10px;
+  width: 500px;
+
+
+  li{
+    list-style:none;
+  }
+`
